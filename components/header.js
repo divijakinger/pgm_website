d1572108c76d4515c48a0f8b5f0f6167896072a4
unknown <divija.kinger@somaiya.edu>
2023-06-19 09:27:27 +0530

    header contents added

--- components/header.js
+++ components/header.js
@@ -5,21 +5,47 @@ class Header extends HTMLElement {
   
     connectedCallback() {
       this.innerHTML = `
-      <nav class="navbar navbar-expand-lg" style="background-color: #9A2929;">
-      <div class="container-fluid text-white">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon text-white" ><i class="bi bi-list" style="color: white;"></i></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-          <a class="navbar-brand text-white" style="font-weight: 1000; font-size: x-large;" href="#">KJSIT</a>
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active text-white" aria-current="page" style="font-weight: 900; font-size:large;" href="#">Probabilistic Graphical Models</a>
-            </li>
-          </ul>
+      <div id="nav-placeholder">
+      <nav class="navbar nav-design" style="background-color: #9A2929;">
+          <div class="container-fluid text-white ">
+            <div>
+              <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#menuOffcanvas" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" >
+              <span class="navbar-toggler-icon">
+              </span>
+              </button>
+              <a class="navbar-brand text-white ms-3" href="#" style="font-weight: 900; font-size:larger;">KJSIT</a>
+            </div>
+            <div class="offcanvas offcanvas-start" tabindex="-1" id="menuOffcanvas" aria-labelledby="menuOffcanvasLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="menuOffcanvasLabel">Contents</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    
+                    <ul>
+                    <a style="text-decoration: none; color:#9A2929;" href="../index.html"><ul>HOME</ul></a>
+                    <hr>
+                    <a style="text-decoration: none; color:#9A2929;" href="../module1/mod1.html"><ul>Introduction TO PGM</ul></a>
+                    <hr>
+                    <a style="text-decoration: none; color:#9A2929;" href="../module2/mod2.html"><ul>Bayesian Network Model and Inference</ul></a>
+                    <hr>
+                    <a style="text-decoration: none; color:#9A2929;" href="../module3/mod3.html"><ul>Markov Network Model and Inference</ul></a>
+                    <hr>
+                    <a style="text-decoration: none; color:#9A2929;" href="../module4/mod4.html"><ul>Hidden Network Model and Inference</ul></a>
+                    <hr>
+                    <a style="text-decoration: none; color:#9A2929;" href="../module5/mod5.html"><ul>Learning and Taking Actions and Decisions</ul></a>
+                    <hr>
+                    <a style="text-decoration: none; color:#9A2929;" href="../module6/mod6.html"><ul>Applications</ul></a>
+                    <hr>
+                    </ul>
+                </div>
+                </div>
+              <div class="d-flex">
+                  <h3 class="nav-text" style="font-weight: 900; font-size:large;">Probabilistic Graphical Models</h3>
+              </div>
+          </div>
+      </nav>
         </div>
-      </div>
-  </nav>
       `;
     }
   }
